488c543db4c5a509ee0a8aa8741eab7880c6849f
israr-mahmood <israr.mahmood.1@gmail.com>
2025-04-26 17:56:13 -0700

    socket broadcast

--- server/socketserver.py
+++ server/socketserver.py
@@ -2,27 +2,44 @@ import asyncio
 import json
 from websockets.asyncio.server import serve
 
+connected_clients = set()
+
 async def handler(websocket):
-    while True:
-        message = await websocket.recv()
-        print(message)
+    connected_clients.add(websocket)
 
-        messageJson = json.loads(message)
-        type = messageJson["type"]
+    try:
+        while True:
+            message = await websocket.recv()
+            print(message)
 
-        if type == "NEW_LOG":
-            await handlerNewLog(websocket, messageJson)
-        elif type == "SET_FLAG":
-            await handlerSetFlag(websocket, messageJson)
+            messageJson = json.loads(message)
+            type = messageJson["type"]
+
+            if type == "NEW_LOG":
+                await handlerNewLog(messageJson)
+            elif type == "SET_FLAG":
+                await handlerSetFlag(messageJson)
+
+        await websocket.wait_closed()
+    finally:
+            connected_clients.remove(websocket)
 
 
-async def handlerSetFlag(websocket, messageJson):
-    await websocket.send(json.dumps(messageJson))
+async def handlerSetFlag(messageJson):
+    await broadcast(json.dumps(messageJson))
     print("Handle new flag complete")
 
-async def handlerNewLog(websocket, messageJson):
-    await websocket.send(json.dumps(messageJson))
+async def handlerNewLog(messageJson):
+    await broadcast(json.dumps(messageJson))
     print("Handle new log complete")
+
+async def broadcast(message):
+    if connected_clients:
+        for client in connected_clients:
+            try:
+                await client.send(message)
+            except:
+                print("failed to send to client")
 
 async def main():
     async with serve(handler, "", 50000) as server:
